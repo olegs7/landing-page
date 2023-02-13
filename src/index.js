4d5f8b58bd10f8a1d062dec1365f502625dca197
Oleg <test@gmail.com>
2023-02-13 18:44:21 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@ import $ from 'jquery';
 import img from './images.js';
 
 $(document).ready(function() {
-let position = 0
 const sliderList = $('.gallary__shoes-list');
 const sliderItem = $('.gallary__shoes-item');
 const arrowLeft = $('.gallary__button-left');
 const arrowRight = $('.gallary__button-right');
+let position = 0;
 
 arrowLeft.click(function(){
-	position -= 285;
-	if(position > 1140){
+	position -= 301.5;
+	if(position > 1210){
 		position = 0
 	}
 	sliderList.css('left', -position);
    })
 
 arrowRight.click(function(){
-	position += 285;
-	if(position > 1140){
+	position += 301.5;
+	if(position > 1210){
 		position = 0 
 	}
 	sliderList.css('left', -position);
