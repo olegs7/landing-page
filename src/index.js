ca57f70de44b860b8cef8f0f0919660b27f4a5db
Oleg <test@gmail.com>
2023-02-14 18:30:38 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -5,23 +5,37 @@ import img from './images.js';
 $(document).ready(function() {
 const sliderList = $('.gallary__shoes-list');
 const sliderItem = $('.gallary__shoes-item');
+const sliderImg = $('.gallary__img');
 const arrowLeft = $('.gallary__button-left');
 const arrowRight = $('.gallary__button-right');
 let position = 0;
 
-arrowLeft.click(function(){
-	position -= 301.5;
-	if(position > 1210){
-		position = 0
-	}
-	sliderList.css('left', -position);
-   })
+const img1 = 'assets/puma-bold.png';
+const img2 = 'assets/puma-sneakers.png';
+const img3 = 'assets/puma-ferrari.png';
+const img4 = 'assets/puma-running.png';
 
-arrowRight.click(function(){
-	position += 301.5;
-	if(position > 1210){
-		position = 0 
+let imgs = [img1,img2,img3,img4]
+
+let i = 0
+
+arrowLeft.click(()=>{
+	if(i > 0){
+		i--
+	}else{
+		i = imgs.length-1
 	}
-	sliderList.css('left', -position);
-  })
+	sliderImg.attr('src',imgs[i])
+})
+
+arrowRight.click(()=>{
+	if(i < imgs.length - 1){
+		i++
+	}else{
+		i = 0
+	}
+	sliderImg.attr('src',imgs[i])
+ })
+
+
 })
