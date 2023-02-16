13ed0622858e0dfcfc63f27f54cb4eaf792bd494
Oleg <test@gmail.com>
2023-02-16 12:55:07 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -3,39 +3,30 @@ import $ from 'jquery';
 import img from './images.js';
 
 $(document).ready(function() {
-const sliderList = $('.gallary__shoes-list');
+// const sliderList = $('.gallary__shoes-list');
 const sliderItem = $('.gallary__shoes-item');
-const sliderImg = $('.gallary__img');
 const arrowLeft = $('.gallary__button-left');
 const arrowRight = $('.gallary__button-right');
-let position = 0;
 
-const img1 = 'assets/puma-bold.png';
-const img2 = 'assets/puma-sneakers.png';
-const img3 = 'assets/puma-ferrari.png';
-const img4 = 'assets/puma-running.png';
+// arrowRight.click(() => {
+//   sliderList.insertBefore(sliderList.lastElementChild, sliderList.firstElementChild);
+//   console.log(sliderList.lastElementChild)
+// });
 
-let imgs = [img1,img2,img3,img4]
+// arrowLeft.click(() => {
+//   sliderList.appendChild(sliderList.firstElementChild);
 
-let i = 0
+// });
 
-arrowLeft.click(()=>{
-	if(i > 0){
-		i--
-	}else{
-		i = imgs.length-1
-	}
-	sliderImg.attr('src',imgs[i])
-})
 
-arrowRight.click(()=>{
-	if(i < imgs.length - 1){
-		i++
-	}else{
-		i = 0
-	}
-	sliderImg.attr('src',imgs[i])
- })
+const sliderList = document.querySelector('.gallary__shoes-list');
+document.querySelector('.gallary__button-right').addEventListener('click', function() {
+  sliderList.insertBefore(sliderList.lastElementChild, sliderList.firstElementChild);
+});
+document.querySelector('.gallary__button-left').addEventListener('click', function() {
+  sliderList.appendChild(sliderList.firstElementChild);
+});
 
 
 })
+
