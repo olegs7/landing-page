266e390de4680b6afe821d8f9d2a965f8a3378e9
Oleg <test@gmail.com>
2023-02-21 17:01:57 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -3,30 +3,51 @@ import $ from 'jquery';
 import img from './images.js';
 
 $(document).ready(function() {
-// const sliderList = $('.gallary__shoes-list');
+const sliderList = $('.gallary__shoes-list');
 const sliderItem = $('.gallary__shoes-item');
 const arrowLeft = $('.gallary__button-left');
 const arrowRight = $('.gallary__button-right');
 
-// arrowRight.click(() => {
+arrowRight.click(() => {
+  sliderList.insertBefore(sliderList.last, sliderList.first);
+ 
+});
+
+arrowLeft.click(() => {
+  sliderList.appendChild(sliderList.first); 
+
+});
+
+
+// const sliderList = document.querySelector('.gallary__shoes-list');
+// document.querySelector('.gallary__button-right').addEventListener('click', function() {
 //   sliderList.insertBefore(sliderList.lastElementChild, sliderList.firstElementChild);
-//   console.log(sliderList.lastElementChild)
 // });
-
-// arrowLeft.click(() => {
+// document.querySelector('.gallary__button-left').addEventListener('click', function() {
 //   sliderList.appendChild(sliderList.firstElementChild);
-
 // });
 
 
-const sliderList = document.querySelector('.gallary__shoes-list');
-document.querySelector('.gallary__button-right').addEventListener('click', function() {
-  sliderList.insertBefore(sliderList.lastElementChild, sliderList.firstElementChild);
-});
-document.querySelector('.gallary__button-left').addEventListener('click', function() {
-  sliderList.appendChild(sliderList.firstElementChild);
-});
+const categoriesItem = $('.gallary__categories-item');
+const gallaryList = $('.gallary__shoes-list');
 
+categoriesItem.each(item => {
+	item.click(() => {
+		let currentItem = item;
+
+		categoriesItem.each(item => {
+			item.classList.remove('active');
+		})
+
+		for(let element of gallaryList){
+			element.classList.add('hidden')
+		}
+
+		currentItem.classList.add('active');
+		const content = $('#' + item.dataset.tab);
+		content.classList.remove('hidden');
+	})
+})
 
 })
 
